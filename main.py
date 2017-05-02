596a98fb2e165d2eccca98b043630e8ba43e16a9
Daniel Trugman <dtrugman@gmail.com>
2017-05-02 19:51:29 +0300

    Add simple main module

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys, logging
+
+from config import Config
+from config import Globals
+
+class WatchDog:
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s :: %(levelname)s :: %(message)s')
+        self._logger = logging.getLogger('watchdog')
+        self._logger.setLevel(logging.DEBUG)
+
+    def _intro(self):
+        self._logger.info("-----------------------------------------------")
+        self._logger.info("WatchDog starting")
+        self._logger.info("-----------------------------------------------")
+        self._logger.info("Version: %s", Globals.VERSION)
+        self._logger.info("Build:   %d", Globals.BUILD)
+        self._logger.info("-----------------------------------------------")
+
+    def _outro(self):
+        self._logger.info("WatchDog stopped")
+
+    def run(self):
+        try:
+            self._intro()
+            self._run()
+        except Exception as ex:
+            self._logger.error(ex.message)
+        finally:
+            self._outro()
+
+    def _run(self):
+        Config.load(sys.argv)
+        config = Config.config
+
+        target = config["target"]
+        freq = config["freq"]
+
+        self._logger.info("Configuration")
+        self._logger.info("-----------------------------------------------")
+        self._logger.info("Target: %s", target)
+        self._logger.info("Freq: %d", freq)
+        self._logger.info("-----------------------------------------------")
+
+if __name__ == "__main__":
+    WatchDog().run()
