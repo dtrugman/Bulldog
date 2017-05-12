b730180feeb51ae2bd83ede4fb91ed96755eceb3
Daniel Trugman <dtrugman@gmail.com>
2017-05-12 17:12:36 +0300

    Add mode to command line args

--- app/manager.py
+++ app/manager.py
@@ -5,9 +5,8 @@ Defines the Manager class
 import sys
 import logging
 import threading
-import time
 
-from app.config import Config
+from app.config_parser import ConfigParser
 from app.version import Version
 
 from app.watchdog import Watchdog
@@ -25,6 +24,7 @@ class Manager(object):
 
         self.stopped = threading.Event()
 
+        self.mode = None
         self.config = None
         self.watchdogs = {}
 
@@ -39,13 +39,18 @@ class Manager(object):
         self.logger.info("Stopped")
 
     def _run(self):
-        self.config = Config.load(sys.argv)
+        config_parser = ConfigParser(sys.argv)
+        self.mode = config_parser.get_mode()
+        self.config = config_parser.get_config()
 
         for app in self.config:
             self.watchdogs[app] = Watchdog(app, self.config[app])
             self.watchdogs[app].start()
 
     def _stop(self):
+        if not self.config:
+            return
+
         for app in self.config:
             if self.watchdogs[app]:
                 self.watchdogs[app].stop()
@@ -59,8 +64,8 @@ class Manager(object):
             self._run()
             #self.stopped.wait()
             raw_input("")
-        except KeyboardInterrupt:
-            self.logger.info("Caught stop request")
+        except Exception as err:
+            self.logger.info("Error!\n%s", err)
         finally:
             self._stop()
             self._outro()
