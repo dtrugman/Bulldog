696103e1011350fcb6c2386cf1c1aadc909fc8ce
Daniel Trugman <dtrugman@gmail.com>
2017-05-12 11:32:43 +0300

    Add create installer script

---
+++ create_installer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script creates an installer for the application
+
+# Step 1: Update version information inside version.py
+VERSION_FILE=app/version.py
+
+# Get build info
+BUILD_NO=`git rev-parse --short HEAD`
+
+# Update values inside the version.py script
+sed -i "s#BUILD = \"[a-zA-Z0-9]\+\"#BUILD = \"$BUILD_NO\"#" $VERSION_FILE
+
+# Step 2: Create the installer
+pyinstaller --onefile watchdog.py
