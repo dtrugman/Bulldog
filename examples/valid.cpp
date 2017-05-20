c29e0e523f0cbe91572262e54add31095eb4b4ff
Daniel Trugman <dtrugman@gmail.com>
2017-05-20 18:09:23 +0300

    Add good behaving example app

---
+++ examples/valid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    while(true)
+    {
+        cout << "HI" << endl;
+        sleep(1000);
+    }
+}
