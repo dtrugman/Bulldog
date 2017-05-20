87d35927d1bfe8e5fa4599d9422ed3d781a95fb8
Daniel Trugman <dtrugman@gmail.com>
2017-05-20 14:14:55 +0300

    Remove REST trigger

--- app/watchdog.py
+++ app/watchdog.py
@@ -5,6 +5,7 @@ Defines the Watchdog class
 import logging
 import threading
 
+from app.triggers.time.cycler import Cycler
 from app.inspector import Inspector
 from app.handler import Handler
 
@@ -15,9 +16,7 @@ class Watchdog(threading.Thread):
 
     KEY_HANDLER = "handler"
     KEY_INSPECTOR = "inspector"
-    KEY_TRIGGERS = "triggers"
     KEY_CYCLER = "cycler"
-    KEY_REST = "rest"
 
     def __init__(self, target_name, config):
         threading.Thread.__init__(self)
@@ -29,7 +28,6 @@ class Watchdog(threading.Thread):
         self.config = config
 
         self.cycler = None
-        self.rest_service = None
 
         self.inspector = None
         self.handler = None
@@ -49,26 +47,12 @@ class Watchdog(threading.Thread):
                                    self.handler)
         self.inspector.start()
 
-        triggers = self.config[Watchdog.KEY_TRIGGERS]
-
-        if Watchdog.KEY_CYCLER in triggers:
-            from app.triggers.time.cycler import Cycler
-            self.cycler = Cycler(self.target_name,
-                                 triggers[Watchdog.KEY_CYCLER],
-                                 self.inspector)
-            self.cycler.start()
-
-        if Watchdog.KEY_REST in triggers:
-            from app.triggers.http.rest import RestService
-            self.rest_service = RestService(self.target_name,
-                                            triggers[Watchdog.KEY_REST],
-                                            self.inspector)
-            self.rest_service.start()
+        self.cycler = Cycler(self.target_name,
+                             self.config[Watchdog.KEY_CYCLER],
+                             self.inspector)
+        self.cycler.start()
 
     def _stop(self):
-        if self.rest_service:
-            self.rest_service.stop()
-
         if self.cycler:
             self.cycler.stop()
 
